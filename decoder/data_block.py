187646e8fa1e47d39799d648d9f8fa77c74f9e9e
emammounqer <emam.anagreh@gmail.com>
2024-05-24 16:45:44 +0300

    fix the laser id in points

--- decoder/data_block.py
+++ decoder/data_block.py
@@ -15,6 +15,42 @@ class DataBlock(NamedTuple):
     data_points: list[Point]
 
 
+packet_order_laser_id_map: dict[int, int] = {
+    1: 31,
+    2: 13,
+    3: 1,
+    4: 17,
+    5: 29,
+    6: 9,
+    7: 3,
+    8: 21,
+    9: 27,
+    10: 12,
+    11: 5,
+    12: 16,
+    13: 25,
+    14: 8,
+    15: 7,
+    16: 20,
+    17: 22,
+    18: 2,
+    19: 10,
+    20: 28,
+    21: 23,
+    22: 6,
+    23: 11,
+    24: 24,
+    25: 18,
+    26: 0,
+    27: 14,
+    28: 30,
+    29: 19,
+    30: 4,
+    31: 15,
+    32: 26,
+}
+
+
 def parse_data_block(data_block: bytes, id: int = -1) -> DataBlock:
     """
     angle in hundredths of a degree
@@ -49,11 +85,15 @@ def parse_data_points(packet_data: bytes) -> list[Point]:
         list[tuple[int, int, int]]: The data points (laser_id, distance, reflectivity)
     """
     data_points: list[Point] = []
-    laser_id = 0
+    order_in_packet = 0
     for i in range(4, 100, 3):
         distance_uncalibrated_mm, reflectivity = struct.unpack_from("< H B", packet_data, i)
-        point = Point(laser_id, distance_uncalibrated_mm * GRANULARITY_MM, reflectivity)
+        point = Point(
+            packet_order_laser_id_map[order_in_packet],
+            distance_uncalibrated_mm * GRANULARITY_MM,
+            reflectivity,
+        )
         data_points.append(point)
-        laser_id += 1
+        order_in_packet += 1
 
     return data_points
